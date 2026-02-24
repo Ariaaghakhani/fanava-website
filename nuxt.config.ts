aff5d9c8fc4dda426fd46c8f2334da92caa53ad4
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2026-02-24 23:18:11 +0330

    Project first initialization

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // Node Version : 24.13
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})

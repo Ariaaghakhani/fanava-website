3582e5c33e347c7d0c4d0ad00005c395a5091017
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2026-02-24 23:29:03 +0330

    Adding ESLint, prettier and main.css and fonts

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h1 class="text-6xl" >Fanava Pouyan Website</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   // Node Version : 24.13
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt',
+  ],
 })
